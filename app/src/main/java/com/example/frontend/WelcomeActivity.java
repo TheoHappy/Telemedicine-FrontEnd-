fdb149a22adaa3fd50cee0c3670f544bffc332df
TheoHappy <teodore473@gmail.com>
2020-11-12 12:07:26 +0200

    i markup welcome an splash page

---
+++ app/src/main/java/com/example/frontend/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package com.example.frontend;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+
+    }
+}
