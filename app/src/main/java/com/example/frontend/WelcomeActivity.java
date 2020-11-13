dda91c8558522adb3338ee565840374c4ecd1b52
TheoHappy <teodore473@gmail.com>
2020-11-13 23:40:58 +0200

    I added hover effect for buttons

--- app/src/main/java/com/example/frontend/WelcomeActivity.java
+++ app/src/main/java/com/example/frontend/WelcomeActivity.java
@@ -8,10 +8,15 @@ import android.widget.Button;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private Button btnSignUp;
+    private Button btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        btnLogin = findViewById(R.id.btn_login);
+//        btnLogin.setTextColor(R.color.colorPrimary);
 
     }
 }
