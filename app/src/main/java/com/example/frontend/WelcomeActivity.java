a2d8f1cca8b37bde23cab10f0abddbed559a80f8
TheoHappy <teodore473@gmail.com>
2020-11-15 20:26:17 +0200

    I added edit texts on login page

--- app/src/main/java/com/example/frontend/WelcomeActivity.java
+++ app/src/main/java/com/example/frontend/WelcomeActivity.java
@@ -2,6 +2,7 @@ package com.example.frontend;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,7 +17,12 @@ public class WelcomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         btnLogin = findViewById(R.id.btn_login);
-//        btnLogin.setTextColor(R.color.colorPrimary);
-
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new  Intent(WelcomeActivity.this, LoginActivity.class);
+                startActivity(loginIntent);
+            }
+        });
     }
 }
